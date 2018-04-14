01648b0b4bba933f12878e73258b86ddf7bf516a
Evgeniy Reykh <evreykh@avito.ru>
2018-04-14 09:34:32 +0300

    Added config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+alias gst='git status'
+alias ga='git add'
+alias gci='git commit -m'
+alias gp='git pull && git push'
+alias gull='git pull'
+alias gush='git push'
+alias gb='git branch'
+alias gco='git checkout'
+alias gd='git diff'
+alias gha='git log --graph --date=relative --all --topo-order --pretty=format:'\''%C(cyan)[%an]%Creset %C(green bold)%d%Creset %C(yellow)%h%Creset : %s %C(cyan)[%ad]%Creset'\'''
+
+alias pu="phing git-update"
+alias puf="phing git-update-full"
+
+alias ping="grc ping"
+alias traceroute="grc traceroute"
+alias make="grc make"
+alias diff="grc diff"
+alias cvs="grc cvs"
+alias netstat="grc netstat"
+alias ifconfig="grc ifconfig"
+alias logc="grc cat"
+alias logt="grc tail"
+alias logh="grc head"
